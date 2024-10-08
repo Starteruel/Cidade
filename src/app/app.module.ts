2cb7a8a5a970f795066da8773ae4824bdc003ec5
starteruel <starteruel@gmail.com>
2024-10-08 11:04:33 -0300

    Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IdCidadeComponent } from './id-cidade/id-cidade.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IdCidadeComponent,
-    CadastroComponent
+    CadastroComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgModule,
     HttpClientModule,
     ReactiveFormsModule
   ],
